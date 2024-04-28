f41eef6d13a82d0e56f1c0d323487d3f6693c2be
tamilvanan31 <tamilvananb31@gmail.com>
2024-04-28 13:31:05 -0700

    first commit

---
+++ src/test/java/com/blocks/msbeerservice/MsBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blocks.msbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
